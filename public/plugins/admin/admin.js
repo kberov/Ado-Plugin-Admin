ece1f510fb04379ffdec34109e70b8619f0a10ca
Krasimir Berov <k.berov@gmail.com>
2015-03-10 01:55:55 +0100

    render_response from ado-users - work in progress

--- public/plugins/admin/admin.js
+++ public/plugins/admin/admin.js
@@ -6,8 +6,15 @@
     /* Bind all the links from the sidebar to list_items */
     $('#admin_menu a[href*="ado"]').each(function(i){
       console.log('binding '+i+': ' + this.href);
-      $(this).click(list_items);
+      $(this).click(render_response);
     });
   }); // end $(document).ready(function($)
 
+  function render_response () {
+    $.get(this.href, function(res) {
+      $('#main_content').html($(res).html());
+      $('title').text($('#tab_title').text())
+    });
+    return false;
+  }
 })(jQuery); //execute;
